335ed1a0e1f8b281316a236a1ba31e3851c0998c
Juan J <juanjose91_blg@hotmail.com>
2023-01-11 14:21:13 -0300

    se limpia archivo de React, y se incluye carrousel, falta editarlo

--- src/index/index.js
+++ src/index/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import '/src/index/index.c7ss';
+import App from './src/app/App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
