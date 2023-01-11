c1c1d87c5a3e158d0e60eb64dea346f2f1321b10
Juan J <juanjose91_blg@hotmail.com>
2023-01-11 14:58:20 -0300

    se limpia archivo react, se agrega carrousel, falta editar

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Carrousel from './Carrousel/Carrousel'
+
+function App() {
+  return (
+    <Carrousel />
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '/src/index/index.c7ss';
-import App from './src/app/App.js';
+import './index.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
